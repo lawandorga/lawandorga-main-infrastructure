16702ea08afa8ad4082b74df0d84b2a969f95d46
Daniel Mössner <38765196+danielmoessner@users.noreply.github.com>
2023-03-18 16:42:21 +0100

    test

--- 02-setup-cluster/i2-providers.tf
+++ 02-setup-cluster/i2-providers.tf
@@ -1,6 +1,6 @@
 provider "scaleway" {
-  access_key = var.scw_access_key
-  secret_key = var.scw_secret_key
+  # access_key = var.scw_access_key
+  # secret_key = var.scw_secret_key
   project_id = var.scw_project_id
   zone       = "fr-par-1"
   region     = "fr-par"
