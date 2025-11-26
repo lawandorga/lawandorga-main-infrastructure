c912c57a236e0bf28c228973f084d18496a44c35
danielmoessner <github@danielmoessner.de>
2025-11-26 22:46:44 +0100

    deps: update cert manager

--- 03-setup-cert-manager/i4-certificates.tf
+++ 03-setup-cert-manager/i4-certificates.tf
@@ -8,7 +8,7 @@ resource "helm_release" "cert_manager" {
   name       = "cert-manager"
   repository = "https://charts.jetstack.io"
   chart      = "cert-manager"
-  version    = "v1.11.0"
+  version    = "v1.19.0"
   namespace  = kubernetes_namespace.cert_manager.metadata.0.name
 
   set {
